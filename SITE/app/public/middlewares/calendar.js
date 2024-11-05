283d58cfe60fac8f2bb130785732340970fce7ed
Gouvea <caua.nascimento@sptech.school>
2024-11-05 01:35:36 -0300

    feat: tela planejamento financeiro

--- SITE/app/public/middlewares/calendar.js
+++ SITE/app/public/middlewares/calendar.js
@@ -3,10 +3,12 @@ const calendar = document.querySelector(".calendar"),
   daysContainer = document.querySelector(".days"),
   prev = document.querySelector(".prev"),
   next = document.querySelector(".next"),
+  todayBtn = document.querySelector(".today-btn"),
   gotoBtn = document.querySelector(".goto-btn"),
   dateInput = document.querySelector(".date-input"),
   eventDay = document.querySelector(".event-day"),
-  eventDate = document.querySelector(".event-date"),
+  // eventDate = document.querySelector(".event-date"),
+  eventDate = document.getElementById("dia-evento"),
   eventsContainer = document.querySelector(".events"),
   addEventBtn = document.querySelector(".add-event"),
   addEventWrapper = document.querySelector(".add-event-wrapper "),
@@ -21,6 +23,9 @@ let activeDay;
 let month = today.getMonth();
 let year = today.getFullYear();
 
+var path = window.location.pathname;
+var fileName = path.split("/").pop();
+
 const months = [
   "Janeiro",
   "Fevereiro",
@@ -33,31 +38,62 @@ const months = [
   "Setembro",
   "Outubro",
   "Novembro",
-  "Dezembro ",
+  "Dezembro",
 ];
 
-// const eventsArr = [
-//   {
-//     day: 13,
-//     month: 11,
-//     year: 2022,
-//     events: [
-//       {
-//         title: "Event 1 lorem ipsun dolar sit genfa tersd dsad ",
-//         time: "10:00 AM",
-//       },
-//       {
-//         title: "Event 2",
-//         time: "11:00 AM",
-//       },
-//     ],
-//   },
-// ];
+let eventsArr = [];
+gerarBagulhoAi();
+function gerarBagulhoAi(){
+  if(fileName != "agendar.html"){
+    eventsArr = [
+      {
+        day: 6,
+        month: 11,
+        year: 2024,
+        events: [
+          {
+            fazenda: "Fazenda X",
+            quantidadeMl: "500 mL",
+          },
+          {
+            fazenda: "Fazenda Y",
+            quantidadeMl: "5000 mL",
+          },
+        ],
+      },
+      {
+        day: 19,
+        month: 11,
+        year: 2024,
+        events: [
+          {
+            fazenda: "Fazenda Z",
+            quantidadeMl: "300 mL",
+          },
+          {
+            fazenda: "Fazenda A",
+            quantidadeMl: "5200 mL",
+          },
+        ],
+      },
+      {
+        day: 29,
+        month: 11,
+        year: 2024,
+        events: [
+          {
+            fazenda: "Fazenda B",
+            quantidadeMl: "800 mL",
+          },
+        ],
+      }
+    ];
+  }
+}
 
-const eventsArr = [];
 // getEvents();
-console.log(eventsArr);
 
+//function to add days in days with class day and prev-date next-date on previous month and next month days and active on today
 function initCalendar() {
   const firstDay = new Date(year, month, 1);
   const lastDay = new Date(year, month + 1, 0);
@@ -92,9 +128,9 @@ function initCalendar() {
       year === new Date().getFullYear() &&
       month === new Date().getMonth()
     ) {
-      // activeDay = i;
-      // getActiveDay(i);
-      // updateEvents(i);
+      activeDay = i;
+      getActiveDay(i);
+      // updateEvents(i)
       if (event) {
         days += `<div class="day today active event">${i}</div>`;
       } else {
@@ -145,7 +181,9 @@ function addListner() {
   const days = document.querySelectorAll(".day");
   days.forEach((day) => {
     day.addEventListener("click", (e) => {
+      getActiveDay(e.target.innerHTML);
       activeDay = Number(e.target.innerHTML);
+      // updateEvents(Number(e.target.innerHTML));
       //remove active
       days.forEach((day) => {
         day.classList.remove("active");
@@ -186,3 +224,60 @@ function addListner() {
     });
   });
 }
+
+//function get active day day name and date and update eventday eventdate
+function getActiveDay(date) {
+  const day = new Date(year, month, date);
+  const dayName = day.toString().split(" ")[0];
+  // eventDay.innerHTML = dayName;
+
+  if(fileName != "agendar.html"){
+    eventDate.innerHTML = date + " " + months[month] + " " + year;
+  }
+}
+
+// function updateEvents(date) {
+//   let events = "";
+//   eventsArr.forEach((event) => {
+//     if (
+//       date === event.day
+//     ) {
+//       console.log("bumbum")
+//       event.events.forEach((event) => {
+//         events += `
+//           <div class="event">
+//               <div>
+//                   <h3 class="event-title">Fazenda X</h3>
+//                   <span class="event-time">-</span>
+//                   <span class="event-time">500 mL</span>
+//               </div>
+//               <div>
+//                   <h3 class="event-icon">
+//                       <i class="fa-solid fa-clipboard-list"></i>
+//                   </h3>
+//               </div>
+//           </div>
+//         `;
+//       });
+//     }
+//   });
+//   if (events === "") {
+//       events = `
+//         <div class="no-event">
+//           <h3>No Events</h3>
+//         </div>
+//       `;
+//   }
+//   if(fileName != "agendar.html"){
+//     eventsContainer.innerHTML = events;
+//   }
+// }
+
+// //function to get events from local storage
+// function getEvents() {
+//   //check if events are already saved in local storage then return event else nothing
+//   if (localStorage.getItem("events") === null) {
+//     return;
+//   }
+//   eventsArr.push(...JSON.parse(localStorage.getItem("events")));
+// }
--- SITE/app/public/middlewares/events.js
+++ SITE/app/public/middlewares/events.js
@@ -6,33 +6,33 @@ function getActiveDay(date) {
   }
   
   //function update events when a day is active
-  function updateEvents(date) {
-    let events = "";
-    eventsArr.forEach((event) => {
-      if (
-        date === event.day &&
-        month + 1 === event.month &&
-        year === event.year
-      ) {
-        event.events.forEach((event) => {
-          events += `<div class="event">
-              <div class="title">
-                <i class="fas fa-circle"></i>
-                <h3 class="event-title">${event.title}</h3>
-              </div>
-              <div class="event-time">
-                <span class="event-time">${event.time}</span>
-              </div>
-          </div>`;
-        });
-      }
-    });
-    if (events === "") {
-      events = `<div class="no-event">
-              <h3>No Events</h3>
-          </div>`;
+function updateEvents(date) {
+  let events = "";
+  eventsArr.forEach((event) => {
+    if (
+      date === event.day &&
+      month + 1 === event.month &&
+      year === event.year
+    ) {
+      event.events.forEach((event) => {
+        events += `<div class="event">
+            <div class="title">
+              <i class="fas fa-circle"></i>
+              <h3 class="event-title">${event.title}</h3>
+            </div>
+            <div class="event-time">
+              <span class="event-time">${event.time}</span>
+            </div>
+        </div>`;
+      });
     }
+  });
+  if (events === "") {
+    events = `<div class="no-event">
+            <h3>No Events</h3>
+        </div>`;
   }
+}
   
   //function to get events from local storage
   function getEvents() {
